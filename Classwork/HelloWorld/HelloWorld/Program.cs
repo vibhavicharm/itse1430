3e88ddf42c3c9c07f0e525bff4760c616c81c62b
pelikankanamala48416 <pelikankanamala48416@NEITD-123517>
2019-01-23 21:24:08 -0600

    Lab2, january 23,2019

--- Classwork/HelloWorld/HelloWorld/Program.cs
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Lab 1 (Lab number)
+ * Vibhavi Jayasinghe (Your name)
+ */
+ // You must have a file header in every lab assignments  
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +15,17 @@ namespace HelloWorld
     {
         static void Main( string[] args )
         {
+            NewGame();
+            DisplayGame();
+
+        }
+
+        //first lab day. this function only has csharp basics. Asks the name and say hello with the user's given name
+        private static void CSharpBasics()
+        {
             string name;
             int hours = 8; //8L for long
-            double payRate = 8.25; //8.25 for Float
+            //double payRate = 8.25; //8.25 for Float
 
 
             Console.WriteLine(hours);
@@ -23,9 +36,85 @@ namespace HelloWorld
             //string message = "Hello " + name; // this is working too in c#
             Console.Write("Hello ");
             Console.WriteLine(name);
-                                        
         }
+
+        //new game function. if we need to make game file to 
+        private static void NewGame()
+        {
+            Console.WriteLine("Enter the name: ");
+            name = Console.ReadLine(); // no parameters and rrturn the string whatever user entered
+
+            Console.WriteLine("Do you own this? ");
+            //string owned = Console.ReadLine();
+            owned = ReadBoolean("Owned (Y/N)?");
+
+            //Console.WriteLine("Price? ");
+            //string price = Console.ReadLine();
+            price = ReadDecimal("Price?");
+
+            Console.WriteLine("Publisher: ");
+            publisher = Console.ReadLine();
+
+            //Console.WriteLine("Completed? ");
+            //string completed = Console.ReadLine();
+            completed = ReadBoolean("Completed (Y/N)?");
+        }
+
+        private static void DisplayGame()
+        {
+            string literall = "Hello \"Ashu\""; // \t is tab, \" shows \\ in console
+            string path = "C:\\Windows\\System32";// those \\ are just one when console
+            string path2 = @"C:\Windows\System32"; //verbatim string is an escape sequence
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Price: " + price);
+            Console.WriteLine("Publisher: " + publisher);
+            Console.WriteLine("Owned? " + owned);
+            Console.WriteLine("Completed? " + completed);
+        }
+
+        private static bool ReadBoolean(string message)
+        {
+            Console.WriteLine(message);
+            string result = Console.ReadLine();
+
+            //validate it is a boolean
+            //TODO: Fix this expression 
+            //these shows in task list as TODO
+            if (result == "Y")
+                return true;
+            if (result == "y")
+                return true;
+            if (result == "N")
+                return false;
+            if (result == "n")
+                return false;
+
+            //TODO: Add validation 
+            return false; 
+        }
+
+        private static decimal ReadDecimal( string message )
+        {
+            Console.WriteLine(message);
+            string value = Console.ReadLine();
+
+            decimal result;
+            if (Decimal.TryParse(value, out result)) //this will not use in this class again
+                return result;
+
+            return 0;
+        }
+
+        private static string name; //global variables
+        private static string publisher;
+        private static decimal price;
+        private static bool owned;
+        private static bool completed;
+
     }
+
+
 }
 
 //if you want to see the running solution you can add a breaker point(press F9 and you can cancel from same button)
@@ -75,9 +164,21 @@ namespace HelloWorld
  * char 2 bytes
  * bool 1 byte
  * 
- * string 
+ * string (unlimited)
  */
 
 /*
+ * January 23, 2019
+ * Video game; we need 
+ * name-string, publisher-string, completed-bool, price-decimal, owned-bool
+ * 
+ * parameters are variable you use the inside of the function body
+ * arguments are the expression when you are calling the function
+ * 
+ * Decimal.Parse(String) converts to decimal
+ * Decimal.tryparse(string,decimal) converts to bool
+ * bool TryParse( string input, out decimal result ); // this result is output parameter 
+ * 
+ * strings are immutable means cannot be changed. cannot modify exsisting strings
  * 
  */
