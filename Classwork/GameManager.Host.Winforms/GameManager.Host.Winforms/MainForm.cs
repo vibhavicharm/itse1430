e12105330e438283efeb5d948b4dba3208c24d13
pelikankanamala48416 <pelikankanamala48416@NEITD-123517>
2019-02-04 21:37:17 -0600

    Feb 4, 2019

---
+++ Classwork/GameManager.Host.Winforms/GameManager.Host.Winforms/MainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameManager.Host.Winforms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadUI();
+        }
+
+        void LoadUI()
+        {
+            Game game = new Game();
+
+            game.Name = "DOOM";
+            game.Price = 59.99M; // you cannot convert double or float to a decimal. Use capital "M" at the end. M stands for money
+
+        }
+    }
+}
+
+/* February 4, 2019
+ * new means initiating new instance and an object
+ * clases are allow us to hide everything else
+ * instance.member - "." is member operator or field operator. member of same type of instance
+ * 
+ */
---
+++ Classwork/GameManager.Host.Winforms/GameManager/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameManager
+{
+    /// <summary> Represents a game </summary>
+    public class Game
+    {
+        /// <summary> Name of the game </summary>
+        public string Name;
+        public string Publlisher;
+        public decimal Price;
+        public bool Owned;
+        public bool Completed;
+    }
+}
+
+/* February 4, 2019 - Classes
+ * every member of class has an accessibility(visibility) in own class
+ * public accessibility means everybody. if a member is public it is visible for everybody
+ * rectangular select - search this. Be very careful with string literals
+ * private - type only (defalt for members)
+ * classes are accessibility when they are public 
+ * if you didnot modify access modifier this means its internal - assembly only
+ * internal - defalt for types
+ * Class;
+ *  they are nouns - noun based
+ *  always pascal case
+ *  public types and public members are reuired to comments in the labs use <summery> tabs - type /// then studio automatically adds these
+ * 
+ */
